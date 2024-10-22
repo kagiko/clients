11dbaaf4e17effca8a6db4a67050b812d3175b95
Steven Deobald <steven@deobald.ca>
2024-10-22 18:32:27 -0300

    remove all references to 'bitwarden_license' dir
    * more aggressive, so starting on a branch

--- jest.config.js
+++ jest.config.js
@@ -19,10 +19,7 @@ module.exports = {
     "<rootDir>/apps/browser/jest.config.js",
     "<rootDir>/apps/cli/jest.config.js",
     "<rootDir>/apps/desktop/jest.config.js",
-    "<rootDir>/apps/web/jest.config.js",
-    "<rootDir>/bitwarden_license/bit-web/jest.config.js",
-    "<rootDir>/bitwarden_license/bit-cli/jest.config.js",
-    "<rootDir>/bitwarden_license/bit-common/jest.config.js",
+    "<rootDir>/apps/web/jest.config.js"
 
     "<rootDir>/libs/admin-console/jest.config.js",
     "<rootDir>/libs/angular/jest.config.js",
